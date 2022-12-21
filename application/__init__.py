5767362bab1ce44ec557138bfe831f935ce0f48e
Bar Tawil <bruriya.tawil@live.biu.ac.il>
2022-12-21 23:08:23 +0200

    separate to routs (Blueprint)

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_session import Session
 import mysql.connector
-from application.forms import CreateRecipeForm, FindRecipeForm
 
 workshop_db = mysql.connector.connect(
     host='localhost',
@@ -14,9 +13,15 @@ workshop_cursor = workshop_db.cursor()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '2136cdc56a8ad364b251f9bb645d1e56' # Create S.K. for anti csrf attack
+app.config['SECRET_KEY'] = '2136cdc56a8ad364b251f9bb645d1e56'  # Create S.K. for anti csrf attack
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-from application import routs
+from application.users.routs import users
+from application.posts.routs import posts
+from application.main.routs import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
